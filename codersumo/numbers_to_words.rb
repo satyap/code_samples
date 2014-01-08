cd537d7c78dd3c0f0106163effe7a15672ea3226
Satya <github@thesatya.com>
2014-01-07 21:44:37 -0800

    Finally did the numbers-to-words from codersumo.

--- codersumo/numbers_to_words.rb
+++ codersumo/numbers_to_words.rb
@@ -1,20 +1,59 @@
-# numbers to words -- incomplete
-# You can code in Ruby
+class Fixnum
+    def to_word
+        n2w = [nil] + %w[one two three four five six seven eight nine]
+        tens2w = [nil] + %w[ten twenty thirty forty fifty sixty seventy eighty ninety]
+        teens2w = %w[ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen]
+        pos = [nil, nil] + %w[hundred] + %w[thousand] + [nil,nil] + %w[million]
+        if self < 10
+            return n2w[self]
+        end
+        if self < 20
+            return teens2w[self - 10]
+        end
+        words = []
+        digits = self.to_s.split('').reverse
+        if digits[1]
+            tens = n2w[digits[1].to_i]
+            if tens=='one'
+                words << teens2w[digits[0].to_i]
+            else
+                words << n2w[digits[0].to_i]
+                words << tens2w[digits[1].to_i]
+            end
+        else
+            words << n2w[digits[0].to_i]
+        end
+        if self >= 100
+            digits[2..-1].each_with_index do |n, i|
+                words += [ pos[i+2], n.to_i.to_word ]
+            end
+        end
 
-n2w = [""] + %w[
-        one two three four five six seven eight nine
-]
-pos=["", ""] + %w[thousand] + ['','']
-
-while line = gets
-    puts line
-    line.chomp!
-    digits = line.split('').reverse
-    i=0
-    puts digits.map do |d|
-        puts i
-        i+=1
-        [pos[i], n2w[d.to_i]]
-    end.flatten.reverse.join('-')
+        return words.compact.reverse.join('-')
+    end
 end
+
+if $0 == __FILE__
+    while(x=gets.to_i)
+        puts x
+        puts x.to_word 
+    end
+end
+
+__END__
+http://codersumo.com/challenges/4
+
+Numbers to words
+
+Write a function that converts numbers to words. Separate each word by a dash character.
+
+    Example: The number 125 will be converted in:
+
+    one-hundred-twenty-five
+
+Input
+List of numbers. Read from the stdin.
+
+    Output
+For each number, print out the english words
 
---
+++ codersumo/spec/numbers_to_words_spec.rb
@@ -0,0 +1,31 @@
+require './numbers_to_words'
+
+describe "Fixnum#to_word" do
+    it "should spell out the word in numbers" do
+        1.to_word.should == "one"
+        10.to_word.should == "ten"
+        11.to_word.should == "eleven"
+        12.to_word.should == "twelve"
+        13.to_word.should == "thirteen"
+        14.to_word.should == "fourteen"
+        15.to_word.should == "fifteen"
+        16.to_word.should == "sixteen"
+        17.to_word.should == "seventeen"
+        18.to_word.should == "eighteen"
+        19.to_word.should == "nineteen"
+        20.to_word.should == "twenty"
+        21.to_word.should == "twenty-one"
+        32.to_word.should == "thirty-two"
+        43.to_word.should == "forty-three"
+        54.to_word.should == "fifty-four"
+        65.to_word.should == "sixty-five"
+        76.to_word.should == "seventy-six"
+        87.to_word.should == "eighty-seven"
+        98.to_word.should == "ninety-eight"
+        99.to_word.should == "ninety-nine"
+        100.to_word.should == "one-hundred"
+        104.to_word.should == "one-hundred-four"
+        114.to_word.should == "one-hundred-fourteen"
+        1354.to_word.should == "one-thousand-three-hundred-fifty-four"
+    end
+end
