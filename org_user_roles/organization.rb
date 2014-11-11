b548035275299467029ac557ac4ddab0969fe1b4
Satya <github@thesatya.com>
2014-11-11 12:46:20 -0800

    Optimize the org roles lookup a bit around 'denied' roles.

--- org_user_roles/organization.rb
+++ org_user_roles/organization.rb
@@ -16,9 +16,21 @@ class Organization < ActiveRecord::Base
     has_role?('denied', user)
   end
 
+  private
   def has_role?(role_name, user)
-    mine = users.merge(Role.send(role_name)).select('id').include? user
-    parents = parent && parent.send("#{role_name}?", user)
-    return mine || !!parents
+    # If denied, stop right here and don't check further
+    if has_role_with_me?('denied', user)
+      return role_name=='denied' ? true : false
+      # If 'denied' role is being checked, return true here.
+      # If 'denied' role is not being checked, return false here.
+      # Could just return the result of the comparison, but that would be more
+      # confusing than returning the explicit values.
+    end
+    mine = has_role_with_me?(role_name, user)
+    return mine || !!(parent && parent.send("#{role_name}?", user))
+  end
+
+  def has_role_with_me?(role_name, user)
+    users.merge(Role.send(role_name)).select('id').include? user
   end
 end
